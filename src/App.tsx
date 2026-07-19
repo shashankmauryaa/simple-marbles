9f1486d4a59daa96fc3f1e652459cc211d1c3331
Shashank Maurya <shashankmaurya7539@gmail.com>
2026-07-19 15:31:56 +0530

    style: apply consistent code formatting and prettier standards to App component imports and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import AboutModal from './components/AboutModal';
-import TermsModal from './components/TermsModal';
-import PrivacyModal from './components/PrivacyModal';
-import Hero from './components/Hero';
-import Collection from './components/Collection';
-import Gallery from './components/Gallery';
-import Process from './components/Process';
-import Footer from './components/Footer';
-import FloatingWhatsApp from './components/FloatingWhatsApp';
-import ProductDetail from './pages/ProductDetail';
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import AboutModal from "./components/AboutModal";
+import TermsModal from "./components/TermsModal";
+import PrivacyModal from "./components/PrivacyModal";
+import Hero from "./components/Hero";
+import Collection from "./components/Collection";
+import Gallery from "./components/Gallery";
+import Process from "./components/Process";
+import Footer from "./components/Footer";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
+import ProductDetail from "./pages/ProductDetail";
 
 interface HomeProps {
   onOpenAbout: () => void;
@@ -25,7 +25,11 @@ function Home({ onOpenAbout, onOpenTerms, onOpenPrivacy }: HomeProps) {
       <Collection />
       <Gallery />
       <Process />
-      <Footer onOpenAbout={onOpenAbout} onOpenTerms={onOpenTerms} onOpenPrivacy={onOpenPrivacy} />
+      <Footer
+        onOpenAbout={onOpenAbout}
+        onOpenTerms={onOpenTerms}
+        onOpenPrivacy={onOpenPrivacy}
+      />
     </>
   );
 }
@@ -40,15 +44,23 @@ function App() {
       <Navbar onOpenAbout={() => setIsAboutOpen(true)} />
       <AboutModal isOpen={isAboutOpen} onClose={() => setIsAboutOpen(false)} />
       <TermsModal isOpen={isTermsOpen} onClose={() => setIsTermsOpen(false)} />
-      <PrivacyModal isOpen={isPrivacyOpen} onClose={() => setIsPrivacyOpen(false)} />
+      <PrivacyModal
+        isOpen={isPrivacyOpen}
+        onClose={() => setIsPrivacyOpen(false)}
+      />
       <FloatingWhatsApp />
-      
+
       <Routes>
-        <Route path="/" element={<Home 
-          onOpenAbout={() => setIsAboutOpen(true)} 
-          onOpenTerms={() => setIsTermsOpen(true)}
-          onOpenPrivacy={() => setIsPrivacyOpen(true)}
-        />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              onOpenAbout={() => setIsAboutOpen(true)}
+              onOpenTerms={() => setIsTermsOpen(true)}
+              onOpenPrivacy={() => setIsPrivacyOpen(true)}
+            />
+          }
+        />
         <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </div>
